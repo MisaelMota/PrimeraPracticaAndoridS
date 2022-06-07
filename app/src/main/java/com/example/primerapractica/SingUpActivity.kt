873462698dcd48fa4dbedfce73ee4d0276929e16
MisaelMota <87102180+MisaelMota@users.noreply.github.com>
2022-06-07 12:54:34 -0400

    Version con la autenticación funcionando pero sin suficientes validaciones

--- app/src/main/java/com/example/primerapractica/SingUpActivity.kt
+++ app/src/main/java/com/example/primerapractica/SingUpActivity.kt
@@ -4,11 +4,38 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SingUpActivity : AppCompatActivity() {
+
+
+    private lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_up)
+
+        auth=FirebaseAuth.getInstance();
+
+        val btnSingUp= findViewById<Button>(R.id.btnSingUp)
+        val txtUsername= findViewById<EditText>(R.id.rEmail)
+        val txtPassword= findViewById<EditText>(R.id.rPassword)
+
+        btnSingUp.setOnClickListener {
+            if(txtUsername.text.trim().toString().isNotEmpty() || txtPassword.text.trim().toString().isNotEmpty()){
+
+                createUser(txtUsername.text.trim().toString(),txtPassword.text.trim().toString())
+
+            }
+            else{
+                Toast.makeText(this,"Imput Required",Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
 
 
         val buttonlog= findViewById<Button>(R.id.AlCount)
@@ -18,6 +45,21 @@ class SingUpActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    fun createUser(email:String,password:String){
+
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){task ->
+                if(task.isSuccessful){
+                    Toast.makeText(this,"Sing Up complete successful",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this,"the password should be have 6 characters min",Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+
 
 
 }
