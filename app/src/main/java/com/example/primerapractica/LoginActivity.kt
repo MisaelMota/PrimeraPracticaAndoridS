618b02fa4a43227977e65dd7a2baa7886adaf501
MisaelMota <87102180+MisaelMota@users.noreply.github.com>
2022-06-07 10:46:35 -0400

    Version con interfaces y actividades creadeas

---
+++ app/src/main/java/com/example/primerapractica/LoginActivity.kt
@@ -0,0 +1,30 @@
+package com.example.primerapractica
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        /*val buttonSin= findViewById<Button>(R.id.NewUser)
+
+        buttonSin.setOnClickListener{
+            val intent= Intent(applicationContext,SingUpActivity::class.java).apply{}
+            startActivity(intent)
+        }*/
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/primerapractica/SingUpActivity.kt
@@ -0,0 +1,23 @@
+package com.example.primerapractica
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SingUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+
+
+        /*val buttonlog= findViewById<Button>(R.id.AlCount)
+
+        buttonlog.setOnClickListener{
+            val intent= Intent(applicationContext,LoginActivity::class.java).apply{}
+            startActivity(intent)
+        }*/
+    }
+
+
+}
